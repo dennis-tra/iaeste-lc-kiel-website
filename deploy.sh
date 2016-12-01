20bf29717f7b9ab4dace94d2ceae453738d74e1f
Dennis Trautwein <dennis.trautwein@posteo.de>
2016-12-01 21:23:14 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 STATUS=$(git status)
 
-if [[ $STATUS =~ "nothing to commit, working directory clean" ]]
+if [[ $STATUS =~ "nothing to commit" ]]
 then
     sed -i "" '/_site/d' ./.gitignore
     git add .
