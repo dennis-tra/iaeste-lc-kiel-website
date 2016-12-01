c555b2f64ebb92443451abd4c860c825162a8087
Dennis Trautwein <dennis.trautwein@posteo.de>
2016-12-01 21:38:34 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 STATUS=$(git status)
+DIST_FOLDER="_site/"
 
 if [[ $STATUS =~ "nothing to commit" ]]
 then
     sed -i "" '/_site/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix gh-pages master`:gh-pages --force
+    git push origin `git subtree split --prefix=_site/ master`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
