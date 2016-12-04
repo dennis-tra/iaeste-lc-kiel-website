fd3ec99b89cea23042ea12ed8d741183d4784137
Dennis Trautwein <dennis.trautwein@posteo.de>
2016-12-04 20:58:49 +0100

    Finish what is IAESTE section

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,31 +1,38 @@
+var drawCanvases = function() {
+
+    var canvases = document.getElementsByClassName("separator-line-canvas");
+    var lineWidth = 2;
+
+    for (var i = 0; i < canvases.length; i++) {
+      var canvas = canvases[i];
+      var heading = canvas.parentElement;
+      var canvasHeight = 40;
+      var canvasWidth = heading.offsetWidth + 45;
+
+      var xLengthHorizontalLine = 0.65 * canvasWidth;
+
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+
+      var ctx = canvas.getContext("2d");
+
+      ctx.beginPath();
+      ctx.moveTo(0, 2);
+      ctx.lineTo(xLengthHorizontalLine, 2);
+      ctx.lineTo(canvasWidth - 2, canvasHeight - 2);
+
+      ctx.lineCap = "round";
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = '#00416C';
+
+      ctx.stroke();
+    }
+}
+
 $(document).ready(function() {
-
-  var canvases = document.getElementsByClassName("separator-line-canvas");
-  var lineWidth = 2;
-
-  for (var i = 0; i < canvases.length; i++) {
-    var canvas = canvases[i];
-    var heading = canvas.parentElement;
-    var canvasWidth = heading.offsetWidth;
-    var canvasOverlap = 100;
-    var canvasHeight = 50;
-
-    canvas.width = canvasWidth + canvasOverlap;
-    canvas.height = canvasHeight;
-
-    var ctx = canvas.getContext("2d");
-
-    ctx.beginPath();
-    ctx.moveTo(0, 2);
-    ctx.lineTo(canvasWidth, 2);
-    ctx.lineTo(canvasWidth + canvasOverlap - 2, canvasHeight - 2);
-
-    ctx.lineCap = "round";
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#00416C';
-
-    ctx.stroke();
-  }
-
-
+  drawCanvases();
 });
+
+window.onresize = function(event) {
+  drawCanvases();
+};
