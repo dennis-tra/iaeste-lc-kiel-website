490e93abe0366c5ba91018e8e2dce6fd5540283f
Dennis Trautwein <dennis.trautwein@posteo.de>
2016-12-06 23:08:20 +0100

    Adjust canvas line drawing in javascript

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,28 +1,33 @@
 var drawCanvases = function() {
 
     var canvases = document.getElementsByClassName("separator-line-canvas");
-    var lineWidth = 2;
 
     for (var i = 0; i < canvases.length; i++) {
       var canvas = canvases[i];
-      var heading = canvas.parentElement;
-      var canvasHeight = 40;
-      var canvasWidth = heading.offsetWidth + 50;
-
-      var xLengthHorizontalLine = 0.65 * canvasWidth;
-
-      canvas.width = canvasWidth;
-      canvas.height = canvasHeight;
-
       var ctx = canvas.getContext("2d");
 
       ctx.beginPath();
+
+      var heading = canvas.parentElement;
+      var canvasHeight = 40;
+      if (canvas.classList.contains("canvas-what-is-iaeste")) {
+        var canvasWidth = heading.offsetWidth + 200;
+        var xLengthHorizontalLine = 0.8 * canvasWidth;
+        ctx.lineWidth = 4;
+      } else {
+        var canvasWidth = heading.offsetWidth + 50;
+        var xLengthHorizontalLine = 0.65 * canvasWidth;
+        ctx.lineWidth = 2;
+      }
+
+
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
       ctx.moveTo(0, 2);
-      ctx.lineTo(xLengthHorizontalLine, 2);
-      ctx.lineTo(canvasWidth - 2, canvasHeight - 2);
+      ctx.lineTo(xLengthHorizontalLine, 4);
+      ctx.lineTo(canvasWidth - 4, canvasHeight - 4);
 
       ctx.lineCap = "round";
-      ctx.lineWidth = 2;
       ctx.strokeStyle = '#00416C';
 
       ctx.stroke();
